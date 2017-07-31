127953cd713f28fd6e9c2023891e95de2e4079f5
Carlos Montalvo <carlosmontalvo@zetanova.com>
2017-07-30 22:36:41 -0500

    Nueva configuración 2017

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-git submodule update --init
-
 for file in `ls -d .??*`; do
-  if [ "$file" != ".gitmodules" ]
-  then
-    source="$PWD/$file"
-    target="$HOME/$file"
-    if [ -L "$target" -o ! -a "$target" ]; then
-      rm -f $target
-      ln -sv $source $target
-    else
-      echo "$target exists but is not a symlink."
-    fi
+  
+  source="$PWD/$file"
+  target="$HOME/$file"
+  if [ -L "$target" -o ! -a "$target" ]; then
+    rm -f $target
+    ln -sv $source $target
+  else
+    echo "$target exists but is not a symlink."
   fi
+  
 done
 
 cd ~
--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 for file in `ls -d .??*`; do
-  if [ "$file" != ".gitmodules" ]
-  then
-    target="$HOME/$file"
-    if [ -L "$target" -o ! -a "$target" ]; then
-      rm -vf $target
-    else
-      echo "$target exists but is not a symlink."
-    fi
+  
+  target="$HOME/$file"
+  if [ -L "$target" -o ! -a "$target" ]; then
+    rm -vf $target
+  else
+    echo "$target exists but is not a symlink."
   fi
+
 done
